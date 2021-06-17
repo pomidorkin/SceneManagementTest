20a047a3d7eb6168b233c41edb186a734416400e
Pomidorkin <pbriuzgin@gmail.com>
2021-06-17 18:33:36 +0900

    Initial commit

---
+++ SceneManagementTest/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using MLAPI.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        // На данном этапе активная сцена это SampleScene
+        SceneManager.LoadScene("SceneTwo", LoadSceneMode.Additive);
+        StartCoroutine(WaitForSceneLoad(SceneManager.GetSceneByName("SceneTwo")));
+    }
+
+    public void LoadPreviousScene()
+    {
+            SceneManager.UnloadSceneAsync("SceneTwo");
+    }
+
+    // Это корутина ждёт пока сцена загрузится и делает её активной
+    public IEnumerator WaitForSceneLoad(Scene scene)
+    {
+        while (!scene.isLoaded)
+        {
+            yield return null;
+        }
+        SceneManager.SetActiveScene(scene);
+    }
+
+}
